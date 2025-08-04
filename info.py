bd60ed622fe912a75d9bbe92906f2428c0dda739
emotion0852-maker <emotion0852@gmail.com>
2025-08-04 17:59:20 +0600

    Update info.py
    Signed-off-by: emotion0852-maker <emotion0852@gmail.com>

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", "-1002370016695"))
 
 # add admin IDs 11111 2222 3333 and add auth channel IDs -100XXX -100XXX -100XXX
 ADMINS = list(map(int, environ.get('ADMINS', '5470968468').split()))  # List of admin user IDs
-AUTH_CHANNEL = list(map(int, environ.get("AUTH_CHANNEL", "-1002148927348").split()))  # Allowed channels for authorization
+AUTH_CHANNEL = list(map(int, environ.get("AUTH_CHANNEL", "-1002474577823").split()))  # Allowed channels for authorization
 
 # username add without @
 OWNER_USERNAME = environ.get("OWNER_USERNAME", 'toon_senpai')  # Owner's username
