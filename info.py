35f2cc38fd2a290697054ca255f3a0f8b6dc4262
code9xrs-hub <code9x.rs@gmail.com>
2025-11-13 00:55:08 +0600

    Update info.py
    Signed-off-by: code9xrs-hub <code9x.rs@gmail.com>

--- info.py
+++ info.py
@@ -8,7 +8,7 @@ SESSION = environ.get('SESSION', 'Webavbot')  # Pyrogram client session name
 
 API_ID = int(environ.get('API_ID', '23201433'))  # Telegram API ID
 API_HASH = environ.get('API_HASH', 'd9e58b52a273bc1a9cc941e60d92beea')  # Telegram API Hash
-BOT_TOKEN = environ.get('BOT_TOKEN', '8215818126:AAEV0b5jHPkVp0SQHV2MAa51RlnVy1SXNdM')  # Telegram Bot Token
+BOT_TOKEN = environ.get('BOT_TOKEN', '8215818126:AAFPYcSSwdkjWVKZRfIiGthy_-MtOkwsBfg')  # Telegram Bot Token
 
 # 👑, Channels & Logs
 BIN_CHANNEL = int(environ.get("BIN_CHANNEL", '-1003417222211'))  # File storage channel
