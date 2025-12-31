3331f472528f60324667ffb24fb3d8f1a888876c
affanoiaibot-cmd <affanoiaibot@gmail.com>
2025-12-31 21:12:41 +0600

    Update commend.py
    Signed-off-by: affanoiaibot-cmd <affanoiaibot@gmail.com>

--- plugins/commend.py
+++ plugins/commend.py
@@ -170,7 +170,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.message.delete()
     elif query.data == "about":
         buttons = [[
-	    InlineKeyboardButton('💻 sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ', url='https://t.me/+ll6DxqQL1IZkZGM1')
+	    InlineKeyboardButton('💻 ʙᴜʏ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ', url='https://t.me/toon_senpai')
 	],[
             InlineKeyboardButton('• ʜᴏᴍᴇ •', callback_data='start'),
 	    InlineKeyboardButton('• ᴄʟᴏsᴇ •', callback_data='close_data')
